f257a6cc897e9c931511c10263047a05f50484ec
Kmilo <camilinchop@gmail.com>
2015-08-03 18:42:38 -0500

    first resources

---
+++ Producto.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1438645141712" ID="ID_1070027916" MODIFIED="1438645158173" TEXT="Producto">
+<node CREATED="1438645163055" ID="ID_1708358830" MODIFIED="1438645169634" POSITION="right" TEXT="Funcionalidad 1">
+<node CREATED="1438645175391" ID="ID_1296837203" MODIFIED="1438645184037" TEXT="funcionalidad o caracter&#xed;sticas"/>
+<node CREATED="1438645184563" ID="ID_1035398786" MODIFIED="1438645192176" TEXT="funcionalidad o caracter&#xed;stica"/>
+</node>
+<node CREATED="1438645195335" ID="ID_1184312535" MODIFIED="1438645199783" POSITION="left" TEXT="Funcionalidad 2">
+<node CREATED="1438645201408" ID="ID_1193849839" MODIFIED="1438645212372" TEXT="Funcionalidad o caracter&#xed;sticas"/>
+<node CREATED="1438645212815" ID="ID_414049712" MODIFIED="1438645220068" TEXT="Funcionalidad o caracter&#xed;sticas"/>
+</node>
+<node CREATED="1438645222702" ID="ID_1798620470" MODIFIED="1438645226817" POSITION="right" TEXT="Funcionalidad 3">
+<node CREATED="1438645228997" ID="ID_910773793" MODIFIED="1438645239687" TEXT="funcionalidad o caracter&#xed;stica"/>
+<node CREATED="1438645240219" ID="ID_794560149" MODIFIED="1438645245926" TEXT="funcionalidad o caracter&#xed;stica"/>
+</node>
+</node>
+</map>
